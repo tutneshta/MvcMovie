b9508c2eabc1760d84a17e74e3fdb432d91bbac4
tutneshta <tutneshta@yandex.ru>
2023-01-23 13:40:30 +0300

    3

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -14,8 +14,10 @@ public class HelloWorldController : Controller
 
     // 
     // GET: /HelloWorld/Welcome/ 
-    public string Welcome(string name, int numTimes = 1)
+    public IActionResult Welcome(string name, int numTimes = 4)
     {
-        return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
     }
 }
---
+++ MvcMovie/Data/MvcMovieContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+            //Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=.\SQLEXPRESS;Database=MvcMovie;Trusted_Connection=True;Trust Server Certificate=True;");
+        }
+    }
+}
