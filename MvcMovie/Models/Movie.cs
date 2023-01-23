d6028a1fa79e2d58a6724448cb3c10ca44cd1560
tutneshta <tutneshta@yandex.ru>
2023-01-23 16:47:57 +0300

    work with Sql

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,13 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models
 {
     public class Movie
     {
         public int Id { get; set; }
-        public string? Title { get; set; }
-        [DataType(DataType.Date)] public DateTime ReleaseDate { get; set; }
-        public string? Genre { get; set; }
+        [Display(Name = "Название")] public string? Title { get; set; }
+
+        [Display(Name = "Дата релиза")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Жанр")] public string? Genre { get; set; }
+
+        [Display(Name = "Цена")]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
